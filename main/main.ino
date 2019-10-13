65dcdc8f0f8b1578e1d5df392f60e5b3aae8c0f6
MBuffa <buffamatthew@gmail.cmo>
2019-10-13 11:40:14 -0400

    Formatting, got rid of potentiometer input pin setup

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,8 @@
 #include "types.h"
 
-#define MIN_TO_MS(X) (X * 1000UL * 60UL)
-#define SEC_TO_MS(X) (X * 1000UL)
-#define SWITCH_PERIOD SEC_TO_MS(5UL)
+#define MIN_TO_MS( X ) ( X * 1000UL * 60UL )
+#define SEC_TO_MS( X ) ( X * 1000UL )
+#define SWITCH_PERIOD SEC_TO_MS( 5UL )
 
 #define NUM_OF_RELAYS 4U
 #define RELAY_K1_PIN  2U
@@ -33,9 +33,6 @@ static xRELAY_CONFIG gxLookUpRelayConfig[eRELAY_COUNT]
   { 12U, 4U }, //eRELAY_K3
   { 13U, 5U }  //eRELAY_K4
 };
-
-#define PERIOD_POT_PIN 0U
-
 #define RELAY_K1_PIN_EN_PIN 10U
 #define RELAY_K2_PIN_EN_PIN 11U
 #define RELAY_K3_PIN_EN_PIN 12U
@@ -49,14 +46,15 @@ static xRELAY_CONFIG gxLookUpRelayConfig[eRELAY_COUNT]
 #define CHARGER_CONNECTED LOW
 #define CHARGER_DISCONNECTED HIGH
 
-#define RELAY_K1_ON (1 << 0)
-#define RELAY_K2_ON (1 << 1)
-#define RELAY_K3_ON (1 << 2) 
-#define RELAY_K4_ON (1 << 3)
+#define RELAY_K1_ON ( 1 << 0U )
+#define RELAY_K2_ON ( 1 << 1U )
+#define RELAY_K3_ON ( 1 << 2U ) 
+#define RELAY_K4_ON ( 1 << 3U )
 
-unsigned long getTimeDifference(unsigned long t0, unsigned long t1);
-unsigned char getEnabledMask(void);
-void updateOutputs (unsigned char outputStateMask);
+unsigned long getTimeDifference( unsigned long t0, unsigned long t1 );
+unsigned char getEnabledMask( void );
+void updateOutputs( unsigned char outputStateMask );
+static void vInitializeRelays( void );
 
 typedef enum relays
 {
@@ -81,31 +79,26 @@ const unsigned char RelayPins [NUM_OF_RELAYS][2] =
   {RELAY_K4_PIN,RELAY_K4_PIN_EN_PIN}
 };
 
-void setup() 
+void setup( void ) 
 {
   vInitializeRelays();
 
-  pinMode(PERIOD_POT_PIN,INPUT);
-
   /*Time selector pins*/
-  pinMode(DIPSWITCH_PIN_B0,INPUT);
-  pinMode(DIPSWITCH_PIN_B1,INPUT);
-  pinMode(DIPSWITCH_PIN_B2,INPUT);
-  pinMode(DIPSWITCH_PIN_B3,INPUT);
+  pinMode( DIPSWITCH_PIN_B0, INPUT );
+  pinMode( DIPSWITCH_PIN_B1, INPUT );
+  pinMode( DIPSWITCH_PIN_B2, INPUT );
+  pinMode( DIPSWITCH_PIN_B3, INPUT );
   
-  digitalWrite(RELAY_K4_PIN,CHARGER_DISCONNECTED);
-  digitalWrite(RELAY_K3_PIN,CHARGER_DISCONNECTED);
-  digitalWrite(RELAY_K2_PIN,CHARGER_DISCONNECTED);//default
-  digitalWrite(RELAY_K1_PIN,CHARGER_CONNECTED);
+  digitalWrite( RELAY_K1_PIN,CHARGER_CONNECTED );
   
-  Serial.println("K1 ON");
+  Serial.println( "K1 ON" );
   startTimeStamps[RELAY_K1] = millis();
   outputStateMask = RELAY_K1_ON;
   
-  Serial.begin(9600);
+  Serial.begin( 9600 );
 }
 
-void loop() 
+void loop( void ) 
 {
 
   currentTime = millis();
@@ -113,47 +106,47 @@ void loop()
 
   //Serial.println(switchPeriod);
   
-  switch (outputStateMask)
+  switch ( outputStateMask )
     {
-      case (RELAY_K1_ON):
-        timeDifference = getTimeDifference(startTimeStamps[RELAY_K1], currentTime);
+      case( RELAY_K1_ON ):
+        timeDifference = getTimeDifference( startTimeStamps[RELAY_K1], currentTime );
         
-        if (timeDifference >= switchPeriod)
+        if( timeDifference >= switchPeriod )
         {
-          digitalWrite(RELAY_K1_PIN,CHARGER_DISCONNECTED);
+          digitalWrite( RELAY_K1_PIN,CHARGER_DISCONNECTED ) ;
           startTimeStamps[RELAY_K1] = 0;
           updateOutputs();
         }
       break;
       
-      case (RELAY_K2_ON):
-        timeDifference = getTimeDifference(startTimeStamps[RELAY_K2], currentTime);
+      case ( RELAY_K2_ON ):
+        timeDifference = getTimeDifference( startTimeStamps[RELAY_K2], currentTime );
 
-        if (timeDifference >= switchPeriod)
+        if( timeDifference >= switchPeriod )
         {
-          digitalWrite(RELAY_K2_PIN,CHARGER_DISCONNECTED);
+          digitalWrite( RELAY_K2_PIN,CHARGER_DISCONNECTED );
           startTimeStamps[RELAY_K2] = 0;
           updateOutputs();
         }
       break;
   
-      case (RELAY_K3_ON):
-        timeDifference = getTimeDifference(startTimeStamps[RELAY_K3], currentTime);
+      case( RELAY_K3_ON ):
+        timeDifference = getTimeDifference( startTimeStamps[RELAY_K3], currentTime );
         
-        if (timeDifference >= switchPeriod)
+        if ( timeDifference >= switchPeriod )
         {
-          digitalWrite(RELAY_K3_PIN,CHARGER_DISCONNECTED);
+          digitalWrite( RELAY_K3_PIN,CHARGER_DISCONNECTED );
           startTimeStamps[RELAY_K3] = 0;
           updateOutputs();
         }
       break;
   
-      case (RELAY_K4_ON):
-        timeDifference = getTimeDifference(startTimeStamps[RELAY_K4], currentTime);
+      case( RELAY_K4_ON ):
+        timeDifference = getTimeDifference( startTimeStamps[RELAY_K4], currentTime );
         
-        if (timeDifference >= switchPeriod)
+        if( timeDifference >= switchPeriod )
         {
-          digitalWrite(RELAY_K4_PIN,CHARGER_DISCONNECTED);
+          digitalWrite( RELAY_K4_PIN,CHARGER_DISCONNECTED );
           startTimeStamps[RELAY_K4] = 0;
           updateOutputs();
         }
@@ -162,47 +155,47 @@ void loop()
     }
 }
   
-void updateOutputs ()
+void updateOutputs( void )
 {
   unsigned char enabledMask;
   enabledMask = getEnabledMask();
   
-  while (1)
+  while( 1 )
   {  
-    if ((outputStateMask << 1) <=  RELAY_K4_ON)
+    if( ( outputStateMask << 1 ) <=  RELAY_K4_ON )
     {
-      outputStateMask = (outputStateMask << 1);
+      outputStateMask = ( outputStateMask << 1 );
     }
     else
     {
       outputStateMask = 1;
     }
         
-    if ((outputStateMask & enabledMask) != 0)
+    if( ( outputStateMask & enabledMask ) != 0)
     {
-      switch (outputStateMask)
+      switch( outputStateMask )
       {
-        case (RELAY_K1_ON):
-          Serial.println("K1 ON ");
-          digitalWrite(RELAY_K1_PIN,CHARGER_CONNECTED);
+        case( RELAY_K1_ON ):
+          Serial.println( "K1 ON " );
+          digitalWrite( RELAY_K1_PIN,CHARGER_CONNECTED );
           startTimeStamps[RELAY_K1] = millis();
         break;
 
-        case (RELAY_K2_ON):
-          Serial.println("K2 ON ");
-          digitalWrite(RELAY_K2_PIN,CHARGER_CONNECTED);
+        case( RELAY_K2_ON ):
+          Serial.println( "K2 ON " );
+          digitalWrite( RELAY_K2_PIN,CHARGER_CONNECTED );
           startTimeStamps[RELAY_K2] = millis();
         break;
 
-        case (RELAY_K3_ON):
-          Serial.println("K3 ON ");
-          digitalWrite(RELAY_K3_PIN,CHARGER_CONNECTED);
+        case( RELAY_K3_ON ):
+          Serial.println( "K3 ON " );
+          digitalWrite( RELAY_K3_PIN,CHARGER_CONNECTED );
           startTimeStamps[RELAY_K3] = millis();
         break;
 
-        case (RELAY_K4_ON):
-          Serial.println("K4 ON ");
-          digitalWrite(RELAY_K4_PIN,CHARGER_CONNECTED);
+        case( RELAY_K4_ON ):
+          Serial.println( "K4 ON " );
+          digitalWrite( RELAY_K4_PIN,CHARGER_CONNECTED );
           startTimeStamps[RELAY_K4] = millis();
         break;
       }
@@ -212,7 +205,7 @@ void updateOutputs ()
   }
 }
 
-unsigned char getEnabledMask(void)
+unsigned char getEnabledMask( void )
 {
   int i;
   unsigned char enabledMask = 0;
@@ -231,35 +224,35 @@ unsigned char getEnabledMask(void)
   return enabledMask;
 }
 
-unsigned long getTimeDifference(unsigned long t0, unsigned long t1)
+unsigned long getTimeDifference( unsigned long t0, unsigned long t1 )
 {
-  if (t1 >= t0)
+  if( t1 >= t0 )
   {
-    return (t1 - t0);
+    return ( t1 - t0 );
   }
   else //just rolled over
   {
-    return ((4294967295 - t0) + t1);
+    return ( ( 4294967295 - t0 ) + t1 );
   }
 }
 
-unsigned long getSwitchPeriod (void)
+unsigned long getSwitchPeriod( void )
 {
 
   unsigned char dipSwitchInput = 0;
   
-  dipSwitchInput |= (digitalRead(DIPSWITCH_PIN_B3) << 3);
-  dipSwitchInput |= (digitalRead(DIPSWITCH_PIN_B2) << 2);
-  dipSwitchInput |= (digitalRead(DIPSWITCH_PIN_B1) << 1);
-  dipSwitchInput |= digitalRead(DIPSWITCH_PIN_B0);
+  dipSwitchInput |= ( digitalRead( DIPSWITCH_PIN_B3 ) << 3 );
+  dipSwitchInput |= ( digitalRead( DIPSWITCH_PIN_B2 ) << 2 );
+  dipSwitchInput |= ( digitalRead( DIPSWITCH_PIN_B1 ) << 1 );
+  dipSwitchInput |= digitalRead( DIPSWITCH_PIN_B0 );
   
   if( dipSwitchInput == 0 )
   {
-    return SEC_TO_MS(1U);
+    return SEC_TO_MS( 1U );
   }
   else
   {
-    return MIN_TO_MS(60U * dipSwitchInput);
+    return MIN_TO_MS( 60U * dipSwitchInput );
   }
 }
 
@@ -271,6 +264,7 @@ static void vInitializeRelays( void )
   {
     pinMode( gxLookUpRelayConfig[eWhichRelay].u8EnablePin, INPUT );
     pinMode( gxLookUpRelayConfig[eWhichRelay].u8TriggerPin, OUTPUT );
+    digitalWrite( gxLookUpRelayConfig[eWhichRelay].u8TriggerPin, CHARGER_DISCONNECTED ); // initialize all relays to be off
   }
 
   return;
